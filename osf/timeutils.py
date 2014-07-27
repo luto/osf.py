9f7a15108687e47a9ae3524c785ecffd63030d6b
luto <m@luto.at>
2014-07-27 17:23:08 +0200

    tests for seconds_to_hhmmss and hhmmss_to_seconds

--- osf/timeutils.py
+++ osf/timeutils.py
@@ -2,14 +2,15 @@ import math
 
 
 def seconds_to_hhmmss(time):
-    seconds = str(time % 60).zfill(2)
+    hundredths = str(round(time % 1, 3))
+    seconds = str(math.floor(time % 60)).zfill(2)
     minutes = str(math.floor((time / 60) % 60)).zfill(2)
     hours = str(math.floor((time / 60 / 60) % 60)).zfill(2)
 
-    return hours + ":" + minutes + ":" + seconds
+    return hours + ":" + minutes + ":" + seconds + "." + hundredths[2:]
 
 
 def hhmmss_to_seconds(hh, mm, ss, hundredths):
     val = int(ss) + int(mm) * 60 + int(hh) * 60 * 60
-    if hundredths: val += int(hundredths) / 1000
+    if hundredths: val += round(int(hundredths) / 1000, 3)
     return val
