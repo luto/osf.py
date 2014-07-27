a1b9bf10de2211a26536eadd1528bb2c11f2166c
luto <m@luto.at>
2014-07-27 17:34:25 +0200

    test OSFLine.__str__

--- osf/timeutils.py
+++ osf/timeutils.py
@@ -2,12 +2,12 @@ import math
 
 
 def seconds_to_hhmmss(time):
-    hundredths = str(round(time % 1, 3))
+    hundredths = str(round(time % 1, 3))[2:].zfill(3)
     seconds = str(math.floor(time % 60)).zfill(2)
     minutes = str(math.floor((time / 60) % 60)).zfill(2)
     hours = str(math.floor((time / 60 / 60) % 60)).zfill(2)
 
-    return hours + ":" + minutes + ":" + seconds + "." + hundredths[2:]
+    return hours + ":" + minutes + ":" + seconds + "." + hundredths
 
 
 def hhmmss_to_seconds(hh, mm, ss, hundredths):
--- test/test_timeutils.py
+++ test/test_timeutils.py
@@ -2,9 +2,34 @@ import pytest
 import osf.timeutils
 
 
-def test_seconds_to_hhmmss():
+def test_seconds_to_hhmmss_1():
     result = osf.timeutils.seconds_to_hhmmss(56011.123)
     assert result == '15:33:31.123'
+
+
+def test_seconds_to_hhmmss_sec_zero_fill():
+    result = osf.timeutils.seconds_to_hhmmss(55981.123)
+    assert result == '15:33:01.123'
+
+
+def test_seconds_to_hhmmss_min_zero_fill():
+    result = osf.timeutils.seconds_to_hhmmss(54211.123)
+    assert result == '15:03:31.123'
+
+
+def test_seconds_to_hhmmss_hr_zero_fill():
+    result = osf.timeutils.seconds_to_hhmmss(18211.123)
+    assert result == '05:03:31.123'
+
+
+def test_seconds_to_hhmmss_hun_zero_fill():
+    result = osf.timeutils.seconds_to_hhmmss(18211.011)
+    assert result == '05:03:31.011'
+
+
+def test_seconds_to_hhmmss_no_hundredths():
+    result = osf.timeutils.seconds_to_hhmmss(18211)
+    assert result == '05:03:31.000'
 
 
 def test_hhmmss_to_seconds():
