b0381b330886e410f06440fb373d97f6c4ae343f
luto <m@luto.at>
2014-07-27 14:00:20 +0200

    added anycast-51 real test (unix timestamps)

--- test/test_real.py
+++ test/test_real.py
@@ -2,10 +2,19 @@ import pytest
 import osf
 import osf.grammar
 
-def test_nsfw_88():
-    f = open('./test/data/nsfw-88.txt')
+
+def do_real_test(file):
+    f = open(file)
     lines = f.readlines()
     f.close()
 
     result = osf.objectify_lines(osf.parse_lines(lines))
     #print("\n".join([str(line) for line in result]))
+
+
+def test_nsfw_88():
+    do_real_test('./test/data/nsfw-88.txt')
+
+
+def test_anycast_51():
+    do_real_test('./test/data/anycast-51.txt')
