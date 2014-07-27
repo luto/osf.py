4b1cbfc9e32137169068a9b4cc54b047f5e983ec
luto <m@luto.at>
2014-07-27 13:09:10 +0200

    OSFLine.__str__

--- osf/classes.py
+++ osf/classes.py
@@ -1,3 +1,13 @@
+import math
+
+
+def seconds_to_hhmmss(time):
+    seconds = str(time % 60).zfill(2)
+    minutes = str(math.floor((time / 60) % 60)).zfill(2)
+    hours = str(math.floor((time / 60 / 60) % 60)).zfill(2)
+
+    return hours + ":" + minutes + ":" + seconds
+
 
 class OSFLine():
     def __init__(self):
@@ -7,3 +17,14 @@ class OSFLine():
         self.tags = []
         self.indentation = 0
 
+    def __str__(self):
+        parts = []
+        if self.indentation:
+            parts.append('-' * self.indentation)
+        if self.time is not None:
+            parts.append(seconds_to_hhmmss(self.time))
+        parts.append(self.text)
+        if self.link:
+            parts.append("<" + self.link + ">")
+        parts.extend(["#" + tag for tag in self.tags])
+        return " ".join(parts)
