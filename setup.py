f5e0b1d52b3ec2504d52713c13be8aba091e3b9b
luto <m@luto.at>
2014-11-19 14:19:34 +0100

    1.11

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='osf',
-      version='1.10',
+      version='1.11',
       description='OSF in python',
       author='luto',
       author_email='m@luto.at',
